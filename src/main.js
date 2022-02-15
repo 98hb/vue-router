5f1536162d0010bf75785756c096df08f81d0c74
‘98hb <w17339377595@163.com>
2022-02-16 04:32:31 +0800

    路由守卫beforeEach

--- src/main.js
+++ src/main.js
@@ -66,6 +66,31 @@ const router = new VueRouter({
   // mode:""  默认不写是"hash"
   // mode: "history"//配合服务器端支持，否则找的是文件夹
 })
+// 目标: 路由守卫
+// 场景: 当你要对路由权限判断时
+// 语法: router.beforeEach((to,from,next)=>{//路由跳转"之前"
+// 先执行这里,决定是否跳转})
+// 参数1: 要跳转到的路由(路由对象信息) 目标
+// 参数2: 从哪里跳转的路由(路由对象信息) 来源
+// 参数3: 函数体 - next()才会让路由正常的跳转切换,
+// next(false)在原地停留,next("强制修改到另一个路由路径上")
+// 注意: 如果不调用next,页面留在原地
+// 例子: 判断用户是否登录,是否决定去"我的音乐"/my
+const isLogin = false;//登陆状态(未登录)
+// const isLogin = true;//登陆状态(已登录)
+router.beforeEach((to, from, next) => {
+  if (to.path === "/my" && isLogin === false) {
+    alert("请登陆")
+    next(false)
+  } else {
+    next()//正常显示
+  }
+})
+// router.beforeEach((to, from, next) => {
+//   console.log(to);//去 to 路由信息对象
+//   console.log(from);//从根来的 "/" from
+//   console.log(next);//函数体
+// })
 Vue.config.productionTip = false
 //6. 路由对象注入到vue实例中,this可以访问$route和$router
 new Vue({
