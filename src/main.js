e3518c8acf5be41a61057fa7c1802d859a13e076
‘98hb <w17339377595@163.com>
2022-02-16 02:03:04 +0800

    展示404页面

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import Find from '@/views/Find'//@是src的绝对地址
 import My from '@/views/My'//@是src的绝对地址
 import Part from '@/views/Part'//@是src的绝对地址
+import NotFound from '@/views/NotFound'//@是src的绝对地址
 // 目标: vue-router基础使用
 // 1. 下载vue-router (yarn add vue-router)
 // 2. 引入
 import VueRouter from "vue-router"
+
 // 3. 注册全局组件
 Vue.use(VueRouter)
 // 4. 规则数组
@@ -30,6 +32,10 @@ const routes = [
   {
     path: "/part/:username",//有 : 的路径  代表要接受具体的值
     component: Part
+  },
+  {//路由404页面必须放在最后 path:"*"
+    path: "*",
+    component: NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>您访问的页面不存在</p>
+    <img src="../assets/404.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+</style>
